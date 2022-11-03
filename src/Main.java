6cdde7eb6b1823aaa4b5fb867c78ccb6658465fd
nst <NineTailSoftware@gmail.com>
2022-11-03 01:10:21 -0400

    test #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My window's name");
+        button = new Button();
+        button.setText("CLICK ME DUMMY!");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
