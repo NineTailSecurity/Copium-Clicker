937e87e1e763c93bf38b0fa03766b545031061c7
nst <NineTailSoftware@gmail.com>
2022-11-04 00:33:06 -0400

    test #3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,52 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Button button;
+    int count = 0;
+    private Label countLabel = new Label();
+
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("My window's name");
+        // stage
+        primaryStage.setTitle("WELCOME TO COPIUM 1.0!");
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250);
+
+        // label
+        countLabel.setText(String.valueOf(count));
+        countLabel.setTextFill(Color.BLUE);
+        countLabel.setStyle("-fx-font-size: 4em;");
+
+        //button create and config
         button = new Button();
         button.setText("CLICK ME DUMMY! 2.0");
+        button.setOnAction(e -> count++);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        VBox vb = new VBox(20);
+        vb.setAlignment(Pos.CENTER);
+        vb.setPrefWidth(scene.getWidth());
+        vb.setLayoutY(30);
+        vb.getChildren().addAll(button, countLabel);
+        root.getChildren().add(vb);
 
-        Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
 
